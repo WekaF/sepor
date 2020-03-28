350ca44e541c5a09d9b140c6daa031d36470f114
WekaF <kenterkasteken@gmail.com>
2020-03-28 11:00:50 -0700

    rev 8

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
           $this->call(UserTableSeeder::class);
           $this->call(jeniskeretasTableSeeder::class);
           $this->call(KategorisTableSeeder::class);
-
-
-
+          
     }
 }
