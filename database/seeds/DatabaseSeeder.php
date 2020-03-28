d2dce5e55b15241bbaea9a392c7a6b16ebbde386
WekaF <kenterkasteken@gmail.com>
2020-03-28 10:43:41 -0700

    rev 7

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
           $this->call(UserTableSeeder::class);
-          $this->call(jeniskeretasSeeder::class);
+          $this->call(jeniskeretasTableSeeder::class);
           $this->call(KategorisTableSeeder::class);
-          $this->call(SubkategoriSeeder::class);
+
 
 
     }
