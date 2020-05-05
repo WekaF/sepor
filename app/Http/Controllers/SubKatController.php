c7b44c703c83dd1ba6f3cb19f4b3b28389548063
002habebcool <habebc@gmail.com>
2020-05-06 06:39:28 +0800

    split route

--- app/Http/Controllers/SubKatController.php
+++ app/Http/Controllers/SubKatController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\SubKategori;
 use App\Kategori;
 use App\Http\Requests;
@@ -21,38 +22,36 @@ class SubKatController extends Controller
     private $subkate;
 
 
-    public function subkat(){
+    public function subkat()
+    {
 
         $subkat = SubKategori::get();
 
         $response['status'] = 'OK';
         $response['result'] = $subkat;
-      
+
         return Response::json($response);
 
         // $data = SubKategori::all();
-      
+
         // return response()->json($data,200);
 
     }
-    
+
     public function index(Request $request)
 
     {
 
-        
-        
+
+
 
         // $data = SubKategori::all();
         $subkategori = SubKategori::all();
         $count = SubKategori::count();
-        $gambar = SubKategori::pluck('gambar','id');
+        $gambar = SubKategori::pluck('gambar', 'id');
         //  dd($subkategori->all());
-      
-         return view('subkategori.index',compact('subkategori'))->with('count',$count)->with(json_encode($gambar,true));
-       
-            
-        
+
+        return view('subkategori.index', compact('subkategori'))->with('count', $count)->with(json_encode($gambar, true));
     }
     public function create(Request $request)
     {
@@ -60,30 +59,28 @@ class SubKatController extends Controller
         $kategori = Kategori::get();
 
         return view('subkategori.create')
-        ->with('kategori',$kategori);
-
-     
+            ->with('kategori', $kategori);
     }
     public function edit($id)
     {
-         
+
         $data = SubKategori::findOrFail($id);
-      
+
         $kategori = Kategori::get();
-       
+
         if (empty($data)) {
             Flash::error('Barang not found');
 
             return redirect(route('subkategori.index'));
         }
 
-        return view('subkategori.edit',compact('data'))->with('subkategori', $data)->with('kategori',$kategori);
+        return view('subkategori.edit', compact('data'))->with('subkategori', $data)->with('kategori', $kategori);
     }
     public function store(Request $request)
     {
-    
-      
-        
+
+
+
         // if($request->file('gambar1')) {
         //     $file = $request->file('gambar1');
         //     $dt = Carbon::now();
@@ -115,26 +112,24 @@ class SubKatController extends Controller
         // } else {
         //     $gambar3 = NULL;
         // }
-       
-    
-         $input=$request->all();
-       
- 
+
+
+        $input = $request->all();
+
+
         //  dd($request->all());
-    
 
-        if($request->hasfile('gambar'))
-        {
 
-           foreach($request->file('gambar') as $image)
-           {
-               $name=$image->getClientOriginalName();
-               $image->move(public_path().'/images/destinasi', $name);  
-               $gambar[] = $name;  
-           }
+        if ($request->hasfile('gambar')) {
+
+            foreach ($request->file('gambar') as $image) {
+                $name = $image->getClientOriginalName();
+                $image->move(public_path() . '/images/destinasi', $name);
+                $gambar[] = $name;
+            }
         }
 
-        SubKategori::Create( [
+        SubKategori::Create([
             'nama_subkategori' => $input['nama_subkategori'],
             'deskripsi'        => $input['deskripsi'],
             'long'             => $input['long'],
@@ -144,85 +139,71 @@ class SubKatController extends Controller
             'gambar'           => json_encode($gambar),
         ]);
 
-        
 
-      
 
-       
-    alert()->success('Berhasil.','Data telah ditambahkan!');       
-    return redirect()->route('subkategori.index');
-      
+
+
+
+        alert()->success('Berhasil.', 'Data telah ditambahkan!');
+        return redirect()->route('subkategori.index');
     }
 
     public function show($id)
     {
         // $subkat = SubKategori::find($id);
-        $subkat = SubKategori::with('kat')->where('kategori_id',$id)->get();
-      
-        return Response::json($subkat,200);
-       
+        $subkat = SubKategori::where('kategori_id', $id)->get();
+        $response['status'] = 'OK';
+        $response['result'] = $subkat;
+
+
+        return Response::json($response, 200);
     }
     public function update(Request $request, $id)
     {
         $data = SubKategori::findOrFail($id);
-        $input=$request->all();
+        $input = $request->all();
 
-          if($request->hasfile('gambar'))
-        {
-            $foto = public_path("/images/destinasi/".$data->gambar);
+        if ($request->hasfile('gambar')) {
+            $foto = public_path("/images/destinasi/" . $data->gambar);
             if (File::exists($foto)) {
                 File::delete($foto);
             }
-           foreach($request->file('gambar') as $image)
-           {
-               $name=$image->getClientOriginalName();
-               $image->move(public_path().'/images/destinasi', $name);  
-               $gambar[] = $name;  
-           }
-
-        }else{
+            foreach ($request->file('gambar') as $image) {
+                $name = $image->getClientOriginalName();
+                $image->move(public_path() . '/images/destinasi', $name);
+                $gambar[] = $name;
+            }
+        } else {
             $name = $data->nama_subkategori;
         }
 
-          $data->nama_subkategori = $request->nama_subkategori;
-          $data->deskripsi = $request->deskripsi;
-          $data->long = $request->long;
-          $data->lat = $request->lat;
-          $data->no_telp = $request->no_telp;
-          $data->kategori_id = $request->kategori_id;
-          $data->gambar = json_encode($gambar);
-          $data->save();
-       
-                
-        return redirect()->route('subkategori.index',compact('data'));
+        $data->nama_subkategori = $request->nama_subkategori;
+        $data->deskripsi = $request->deskripsi;
+        $data->long = $request->long;
+        $data->lat = $request->lat;
+        $data->no_telp = $request->no_telp;
+        $data->kategori_id = $request->kategori_id;
+        $data->gambar = json_encode($gambar);
+        $data->save();
+
+
+        return redirect()->route('subkategori.index', compact('data'));
     }
 
     public function destroy($id)
     {
-       
+
         $kategori = \App\SubKategori::find($id);
         $kategori->delete();
-        alert()->success('Berhasil.','Data telah dihapus!');
+        alert()->success('Berhasil.', 'Data telah dihapus!');
         return redirect()->route('subkategori.index');
-        
-       }
+    }
 
-       public function list($id,$detail){
+    public function list($id)
+    {
+        $subkat = SubKategori::where('id', $id)
+            ->get();
 
-       
-        $subkat = SubKategori::with('kat')
-                             ->where('kategori_id',$id)
-                             ->where('id',$detail)
-                             ->get();
-        
-        return Response::json($subkat,200);
-
-
-
-        
-       }
-
-      
-   
+        return Response::json($subkat, 200);
+    }
 }
-
--- routes/api.php
+++ routes/api.php
@@ -4,43 +4,44 @@ use Illuminate\Http\Request;
 
 
 
-Route::get('home','HomeController@homej');
+Route::get('home', 'HomeController@homej');
 
-Route::group(array('prefix' => 'subkategori'), function(){
-    Route::get('', 'SubKatController@subkat');
-    Route::get('/{id}', 'SubKatController@show');
-    Route::get('/{id}/{detail}','SubKatController@list');
-   });
-
-Route::group(array('prefix' => 'kategori'), function(){
-    Route::get('', 'KategoriController@kate');
-    Route::get('/{id}', 'KategoriController@show');
-     Route::get('/{id}/{detail}', 'KategoriController@list');
-   
-   });   
-
-Route::group(array('prefix' => 'trayek'), function(){
-    Route::get('', 'TrayekController@trayek');
-    Route::get('/{id}', 'TrayekController@show');
-   });
-   
-Route::group(array('prefix' => 'taxi'), function(){
-    Route::get('', 'TaxiController@taxi');
-    Route::get('/{id}', 'TaxiController@show');
-   });
-
-Route::group(array('prefix' => 'keretainfo'), function(){
-    Route::get('', 'KeretainfoController@infokereta');
-    Route::get('/{id}', 'KeretainfoController@keretashow');
-   });
-
- Route::get('kontak','KontakController@kontak');
-
- Route::get('stasiuninfo','StasiunInfoController@infostat');
+Route::group(array('prefix' => 'subkategori'), function () {
+   Route::get('', 'SubKatController@subkat');
+   Route::get('/{id}', 'SubKatController@show');
+   Route::get('/{id}/{detail}', 'SubKatController@list');
+});
+Route::group(array('prefix' => 'destination'), function () {
+   Route::get('/{id}', 'SubKatController@list');
+});
 
 
- Route::group(array('prefix' => 'berita'), function(){
-    Route::get('','BeritaController@berita');
-    Route::get('/detail/{id}','BeritaController@show'); 
- });
- 
+Route::group(array('prefix' => 'kategori'), function () {
+   Route::get('', 'KategoriController@kate');
+   Route::get('/{id}', 'KategoriController@show');
+   Route::get('/{id}/{detail}', 'KategoriController@list');
+});
+
+Route::group(array('prefix' => 'trayek'), function () {
+   Route::get('', 'TrayekController@trayek');
+   Route::get('/{id}', 'TrayekController@show');
+});
+
+Route::group(array('prefix' => 'taxi'), function () {
+   Route::get('', 'TaxiController@taxi');
+   Route::get('/{id}', 'TaxiController@show');
+});
+
+Route::group(array('prefix' => 'keretainfo'), function () {
+   Route::get('', 'KeretainfoController@infokereta');
+   Route::get('/{id}', 'KeretainfoController@keretashow');
+});
+
+Route::get('kontak', 'KontakController@kontak');
+
+Route::get('stasiuninfo', 'StasiunInfoController@infostat');
+
+Route::group(array('prefix' => 'berita'), function () {
+   Route::get('', 'BeritaController@berita');
+   Route::get('/detail/{id}', 'BeritaController@show');
+});
